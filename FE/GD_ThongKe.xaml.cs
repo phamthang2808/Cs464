762bd8842a37d34facdf7f8056180fba09301644
Tên tài khoản GitHub của bạn <phanthedat@dtu.edu.vn>
2025-09-23 10:27:45 +0700

    thêm sml

---
+++ FE/GD_ThongKe.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FE
+{
+    /// <summary>
+    /// Interaction logic for GD_ThongKe.xaml
+    /// </summary>
+    public partial class GD_ThongKe : Window
+    {
+        public GD_ThongKe()
+        {
+            InitializeComponent();
+        }
+    }
+}
